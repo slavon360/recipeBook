90b382dbfb54e28d0066a4dc49810df0ba6a3784
Viacheslav <slavon360@gmail.com>
2017-02-24 23:24:44 +0200

    c

--- app_client/views/works/weatherApp/weatherAppCtrl.js
+++ app_client/views/works/weatherApp/weatherAppCtrl.js
@@ -10,7 +10,7 @@
      }
      $scope.submitForm=function(){
       console.log($scope.weather.city)
-       $http.get('https://api.openweathermap.org/data/2.5/weather?q='+$scope.weather.city+'&units=metric&APPID=8d9223b647133c51d397626dcaa319ce')
+       $http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.weather.city+'&units=metric&APPID=8d9223b647133c51d397626dcaa319ce')
        .then(function(response){
         console.log(response);
          $scope.cities.push(response.data);
